bc64a33fa3ab3c070828ba3f5b1e95832ec40d09
Comondear <104315742+Comondear@users.noreply.github.com>
2022-09-30 20:57:48 +0000

    Deploying to gh-pages from @ Comondear/goit-js-hw-09@fbe7ebb7c9216357f8eabd3c1286efde41c42407 🚀

---
+++ 01-color-switcher.65527525.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")},e=null;t.start.addEventListener("click",(function(){t.start.disabled=!0,e=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.stop.addEventListener("click",(function(){clearInterval(e),t.start.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.65527525.js.map
---
+++ 01-color-switcher.828ce024.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")};let e=null;t.start.addEventListener("click",(function(){t.start.disabled=!0,e=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(function(){clearInterval(e),t.start.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.828ce024.js.map
